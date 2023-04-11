ee69c348c9cb9a3ff0ac74408a35550c02066992
jacksonFosseen <jackson.fosseen@wsu.edu>
2023-04-11 20:13:35 +0000

    required fields applied

--- scripts.js
+++ scripts.js
@@ -13,9 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("https://weatherapi-com.p.rapidapi.com/current.json?q=99163", options)
     .then(response => response.json())
     .then(function(response) {
+        document.querySelector("#location span").innerHTML = response.location.name;
         document.querySelector("#tempF span").innerHTML = response.current.temp_f;
         document.querySelector("#tempC span").innerHTML = response.current.temp_c;
+        document.querySelector("#winDir span").innerHTML = response.current.wind_dir;
+        document.querySelector("#winSpeed span").innerHTML = response.current.wind_mph;
+        document.querySelector("#cloud span").innerHTML = response.current.cloud;
+        document.querySelector("#humidity span").innerHTML = response.current.humidity;
+        console.log(response);
         return response;
     })
     .catch((err) => console.error(err));
 });
+
